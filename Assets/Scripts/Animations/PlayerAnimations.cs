3c6d91437ee7d10e8bcfc8c8863a850d82670936
Dachy Mathieu <matt.dachy@gmail.com>
2017-02-04 20:56:05 +0100

    Modify player prefab controller

---
+++ Assets/Scripts/Animations/PlayerAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Animator m_animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_animator = GetComponent<Animator>();
+        m_animator.SetTrigger("LeftIdle");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            m_animator.SetTrigger("LeftRun");
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            m_animator.SetTrigger("LeftIdle");
+        }
+	}
+}
