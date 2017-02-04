159279fe94cb46b3e7eba0be66ae4a641ff45430
Jackamikaz <camille.bartalan@gmail.com>
2017-02-04 17:55:54 +0100

    Zombie ajouté et script qui fait face à la caméra

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	void Update() 
+	{
+		Vector3 pos = new Vector3();
+		pos += transform.position + Camera.main.transform.forward;
+		transform.LookAt(pos, Vector3.up);
+	}
+}
